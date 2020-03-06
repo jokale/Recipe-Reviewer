be77f590cd6b3815ab0eaccb1999b86781c5a3a6
jokale <kalemajoanna@gmail.com>
2020-03-06 18:15:20 +0000

    fix log in page and create flash error and authenticate so if user enters incorrect password they get given an error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,24 @@ class SessionsController < ApplicationController
     end 
 
     def new 
+
     end 
 
     def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+             redirect_to user_path(@user)
+        else 
+             flash[:error] = "Sorry that was incorrect, please try again"
+             redirect_to login_path
+            # redirect_to '/'
+
+            #user is somehow being saved but is not redirecting to the correct route!!!
+        end 
     end 
 
+    
     def destroy 
         session.delete(:user_id)
         redirect_to '/'
