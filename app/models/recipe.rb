d17a9790d7391eb5178dc55441d60add4e081492
jokale <kalemajoanna@gmail.com>
2020-03-06 09:50:08 +0000

    correct ar associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
     has_many :users
+    has_one :meal 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews 
     has_many :meals, through: :recipes 
+    has_many :recipes 
 end
