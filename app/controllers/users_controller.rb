32e573b2e5368e6e35f22ff1895bfdffe204abee
jokale <kalemajoanna@gmail.com>
2020-03-06 15:38:48 +0000

    add bcrypt and validate username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,20 @@ def new
 end 
 
 def create
+    @user = User.new(user_params)
+    if @user.save 
+    session[:user_id] = @user.id 
+    redirect_to user_path(@user)
+
+def show 
 end 
+
+
+
+private 
+
+def user_params
+    params.require(:user).permit(:username,:email,:password)
+end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :reviews 
     has_many :meals, through: :recipes 
     has_many :recipes 
+
+    validates :username, presence: true, uniqueness:trueb
+    has_secure_password
 end
