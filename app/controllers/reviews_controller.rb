8e1c8d6728d71334886db0bfcea67e38292879a3
jokale <kalemajoanna@gmail.com>
2020-03-07 16:26:57 +0000

    make rating a numerical field

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,11 @@
 class ReviewsController < ApplicationController
 
 def new
+    @review = Review.new 
+    @recipe = Recipe.find_by_id(params[:id])
+    #  @review = @recipe.review.build
+    # page loads when that line of code is commented out
+    # undefined method `build' for nil:NilClass
 end 
 
 def index 
