6c3cd8ab95ffb30a81e48d623733bb99976532fc
jokale <kalemajoanna@gmail.com>
2020-03-10 10:57:22 +0000

    fix error where user was not able to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,29 +12,29 @@ class SessionsController < ApplicationController
         
 
         def create
-            if request.env[‘omniauth.auth’]
+            if request.env['omniauth.auth']
               user = User.create_with_omniauth(request.env[‘omniauth.auth’])
               session[:user_id] = user.id    
               redirect_to user_path(user.id)
             else
-              user = User.find_by_email(params[:email])
-              user && user.authenticate(params[:password])
-              session[:user_id] = user.id
-              redirect_to user_path(user.id)
-            end
-          end
-    #     @user = User.find_by(username: params[:user][:username])
+          #     user = User.find_by_email(params[:email])
+          #     user && user.authenticate(params[:password])
+          #     session[:user_id] = user.id
+          #     redirect_to user_path(user.id)
+          #   end
+          # end
+         @user = User.find_by(username: params[:user][:username])
     #     if @user.try(:authenticate, params[:user][:password])
-    #         session[:user_id] = @user.id
-    #          redirect_to user_path(@user)
-    #     else 
-    #          flash[:error] = "Sorry that was incorrect, please try again"
-    #          redirect_to login_path
-    #         # redirect_to '/'
+           session[:user_id] = @user.id
+              redirect_to user_path(@user)
+        # else 
+        #      flash[:error] = "Sorry that was incorrect, please try again"
+        #       redirect_to login_path
+        #      redirect_to '/'
 
     #         #user is somehow being saved but is not redirecting to the correct route!!!
-    #     end 
-    # end 
+         end 
+    end 
 
     
     def destroy 
